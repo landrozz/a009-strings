fe92405f866725c9e7c5102e80c66d9e304e4068
landrozz <leandrozanobine@gmail.com>
2022-09-01 11:07:35 -0300

    exercicios realizados

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair."`
+const fraseTrocada = frase.replaceAll("verde","rosa").replaceAll("azul","laranja")
+const fraseTrocadaMaiuscula = fraseTrocada.replace("mas não deixe o gato sair", "MAS NAO DEIXE O GATO SAIR")
+
+console.log(frase)
+console.log(fraseTrocada)
+console.log(`A segunda string inclui a palavra "verde"? - ${fraseTrocada.includes("verde")}
+A segunda string inclui a palavra "laranja"? - ${fraseTrocada.includes("laranja")}`)
+console.log(fraseTrocadaMaiuscula)
